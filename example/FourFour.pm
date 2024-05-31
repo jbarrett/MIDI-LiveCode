281d846865ae55a491aee24faf78ac45ad754a5a
John Barrett <john@jbrt.org>
2024-05-31 16:45:21 +0100

    Updating example to use genmidi type devices

--- example/FourFour.pm
+++ example/FourFour.pm
@@ -2,23 +2,33 @@ package FourFour;
 
 use MIDI::LiveCode;
 
+device qr/wavetable|timidity/i;
 tempo 120;
 midi_bits 7;
 
 loop kick => sub {
     every 'qn';
     trigger;
-    channel 1;
+    channel 10;
     note 'c5';
     vel 127;
+};
+
+loop snare => sub {
+    every 'hn';
+    offset 'qn';
+    trigger;
+    channel 10;
+    note 'c#5';
+    vel random 110 => 127;
 };
 
 loop hat => sub {
     every 'sn';
     trigger;
-    channel 1;
+    channel 10;
     note 'd5';
-    vel random 80,  127;
+    vel random 80 => 127;
 };
 
 finalize;
