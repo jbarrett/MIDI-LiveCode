c31d84a5f50c9aeaa5ec9b13e3e917326b7ce6cd
John Barrett <john@jbrt.org>
2024-06-14 18:40:02 +0100

    Add score finalisation and module reloading to Event stash

--- lib/MIDI/LiveCode/Events.pm
+++ lib/MIDI/LiveCode/Events.pm
@@ -1,49 +1,52 @@
-use v5.38;
+use v5.40;
 use experimental qw/ class /;
 my $instances;
 
-class MIDI::LiveCode::Events {
+class MIDI::LiveCode::Events :isa( MIDI::LiveCode::Decor ) {
 
+    use again;
     use Carp qw/ carp croak /;
 
     field $module :param;
-    field $events;
+    field $events :reader;
 
     sub for_module( $class, $module ) {
-        $instances->{ $module } //
+        $instances->{ $module } //=
         $class->new( module => $module );
     }
 
-    method push_events( $name, $event ) {
+    method push_events :SkipIfFinalized ( $name, $event ) {
         croak "Event $name already exists"
             if exists $events->{ events }->{ $name };
         $events->{ events }->{ $name } = $event;
     }
 
-    method push_config( $cfg, $val ) {
+    method push_config :SkipIfFinalized ( $cfg, $val ) {
         $events->{ config }->{ $cfg } = $val;
     }
 
-    method config ( $cfg ) {
+    # Need to rationalise : local vs global configs.
+    # Some things are not intechangeable ...
+    method config( $cfg = undef ) {
         $cfg
             ? $events->{ config }->{ $cfg }
             : $events->{ config }
     }
 
-    method finalize {
-        #croak "Events have already been finalized!"
-        carp "Events have already been finalized!"
-            if $events->{ finalized };
-        $events->{ finalized } = 1;
+    method finalize :SkipIfFinalized {
+        $events->{ finalized } = true;
+    }
+
+    method music {
+        $events->{ events };
     }
 
     method init {
         $events = {};
     }
 
-    method events { $events }
-
-    ADJUST {
-        $instances->{ $module } = $self;
+    method reload {
+        $self->init;
+        require_again $module;
     }
 }
