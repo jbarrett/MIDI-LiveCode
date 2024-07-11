66a81c3cc3537dc21e467aec2ff936fa810c2258
John Barrett <john@jbrt.org>
2024-07-11 10:07:30 +0100

    DSL tweak, no longer require blocks, everything is inow a chain of coderefs

--- lib/MIDI/LiveCode/DSL.pm
+++ lib/MIDI/LiveCode/DSL.pm
@@ -3,7 +3,12 @@ package MIDI::LiveCode::DSL;
 use v5.38;
 use experimental qw/ defer for_list /;
 
-use Carp qw/ carp croak /;
+use meta;
+no warnings 'meta::experimental';
+my $meta = meta::get_this_package;
+
+use Carp qw/ croak carp /;
+use MIDI::LiveCode::Events;
 
 my @config = qw/
     device
@@ -15,6 +20,7 @@ my @config = qw/
 
 my @defines = qw/
     loop
+    lfo
     oneshot
 /;
 
@@ -26,15 +32,13 @@ my @defines = qw/
 my @parameters = qw/
     cc
     channel
-    every
+    duration
     tick
-    length
     note
     offset
     probability
     quantize
     swing
-    trigger
     velocity
 /;
 
@@ -43,32 +47,49 @@ my @specials = qw/
     finalize
     midi_bits
     random
+    stop
 /;
 
 my $aliases = { qw/
+    align     quantize
     bpm       tempo
     delay     offset
-    dur       length
-    duration  length
+    dur       duration
+    every     tick
     finalise  finalize
-    len       length
+    len       duration
     pitch     note
     port      device
     ppq       ppqn
     prob      probability
     quantise  quantize
     quant     quantize
-    repeat    every
+    sig       signature
+    trig      trigger
+    trip      triplet
+    tr        triplet
     time      signature
     vel       velocity
 / };
 
-use meta;
-no warnings 'meta::experimental';
-my $meta = meta::get_this_package;
+my $note_lengths;
+my $note_names;
+my $all_notes;
+BEGIN {
+    require MIDI::LiveCode::Notation;
+    $note_lengths = [ keys MIDI::LiveCode::Notation->duration_names->%* ];
+    $note_names = [ MIDI::LiveCode::Notation->note_names->@*,
+                 grep { /^[a-z]+$/i } keys MIDI::LiveCode::Notation->note_aliases->%* ];
 
-use Carp qw/ croak carp /;
-use MIDI::LiveCode::Events;
+    $all_notes = {
+        map {
+            my $octave = $_;
+            map {
+                ( "$_$octave", "$_$octave" )
+            } $note_names->@*
+        } 0..10
+    };
+}
 
 use parent 'Exporter';
 our @EXPORT_OK = our @EXPORT = (
@@ -76,35 +97,27 @@ our @EXPORT_OK = our @EXPORT = (
     @defines,
     @parameters,
     @specials,
+    $note_lengths->@*,
+    keys $all_notes->%*,
     keys $aliases->%*
 );
 
-sub _events {
-    MIDI::LiveCode::Events->for_module( scalar caller(1) );
+my $event;
+my $caller;
+
+sub import {
+    $caller = caller;
+    __PACKAGE__->export_to_level(1, @_);
 }
 
-my $event;
-sub _current_event {
-    my $caller = (caller(0))[3] =~ s/.*://gr;
-    carp "$caller called outside event definition" unless $event;
-    return $event;
+sub _events {
+    MIDI::LiveCode::Events->for_module( $caller );
 }
 
 for my $cfg ( @config ) {
     $meta->add_symbol(
         "&$cfg" => sub( $val ) {
             _events->push_config( $cfg => $val );
-        }
-    )
-}
-
-for my $def ( @defines ) {
-    $meta->add_symbol(
-        "&$def" => sub ( $name, $cb ) {
-            defer { undef $event; }
-            $event = {};
-            $cb->();
-            _events->push_events( $name, [ $def => $event ] );
         }
     )
 }
@@ -121,6 +134,10 @@ for my $param ( @parameters ) {
     )
 }
 
+sub stop {
+    sub {};
+}
+
 sub random( $low, $hi ) {
     carp "'random' called outside event definition" unless $event;
     sub {
@@ -128,16 +145,43 @@ sub random( $low, $hi ) {
     }
 }
 
-sub midi_bits( $bits ) {
+sub midi_bits( $bits, $mode = 'await' ) {
     if ( $event ) {
         return $event->{ midi_bits } = $bits;
     }
     _events->push_config( midi_bits => $bits );
+    _events->push_config( midi_mode => $mode );
 }
 
 sub finalize {
     _events->finalize;
 }
+
+sub _add_event {
+    my ( $type, $dur, $name, $cb ) = ref $_[2] // '' eq 'CODE'
+        ? ( $_[0], 'bar', @_[1..2] )
+        : @_;
+    defer { undef $event; }
+    $event = {};
+    $cb->();
+    _events->push_events( $name, [ $dur, $type => $event ] );
+}
+
+for my $def ( @defines ) {
+    $meta->add_symbol(
+        "&$def" => sub {
+            _add_event( $def, @_ )
+        }
+    )
+}
+
+for my $length ( $note_lengths->@* ) {
+    $meta->add_symbol(
+        "&$length" => sub { $length }
+    );
+};
+
+use constant $all_notes;
 
 for my ( $alias, $orig ) ( $aliases->%* ) {
     $meta->add_symbol(
