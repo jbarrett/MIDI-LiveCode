881d7d69b5a8d83565fa34d7007172e382df0433
John Barrett <john@jbrt.org>
2024-07-11 10:25:34 +0100

    Update example - show more linear syntax, stop keyword, coderef param

--- example/FourFour.pm
+++ example/FourFour.pm
@@ -1,34 +1,36 @@
-package FourFour;
+package FourFourV2;
 
 use MIDI::LiveCode;
 
-device qr/wavetable|timidity/i;
+device qr/loopmidi|timidity/i;
 tempo 120;
 midi_bits 7;
 
-loop kick => sub {
-    every 'qn';
-    trigger;
-    channel 10;
-    note 'c5';
-    vel 127;
-};
+loop bar => kick =>
+#stop
+every qn,
+dur trigger,
+channel 10,
+note c3,
+vel 127;
 
-loop snare => sub {
-    every 'hn';
-    offset 'qn';
-    trigger;
-    channel 10;
-    note 'c#5';
-    vel random 110 => 127;
-};
+loop snare =>
+every hn,
+offset qn,
+dur trigger,
+channel 10,
+note cs3,
+vel random 110 => 127;
 
-loop hat => sub {
-    every 'sn';
-    trigger;
-    channel 10;
-    note 'd5';
-    vel random 80 => 127;
+loop hat => return
+every sn,
+channel 10,
+dur trigger,
+note d3,
+vel sub {
+    state @vels = 70, 80, 60, 90;
+    push @vels, shift @vels;
+    $vels[-1]
 };
 
 finalize;
