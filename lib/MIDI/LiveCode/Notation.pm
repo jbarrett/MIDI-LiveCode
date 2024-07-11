9e1b543c3a5fc7363afff110b8246547845dad41
John Barrett <john@jbrt.org>
2024-07-11 10:11:13 +0100

    Supporting stuff for DSL, add middle-C offset to note resolution fn

--- lib/MIDI/LiveCode/Notation.pm
+++ lib/MIDI/LiveCode/Notation.pm
@@ -3,13 +3,13 @@ package MIDI::LiveCode::Notation;
 use v5.40;
 
 use Carp qw/ carp croak /;
+use Scalar::Util qw/ looks_like_number /;
 
 use aliased 'MIDI::LiveCode::Config' => 'cfg';
 
 use parent 'Exporter';
 our @EXPORT_OK =
 our @EXPORT = qw/
-    duration_ppqn
     duration
     pitch
 /;
@@ -23,14 +23,19 @@ my $duration_names = {
     en => 1/2,
     sn => 1/4,
     tn => 1/8,
-    tr => 2/3,
+    triplet => 2/3,
+    trigger => 1/64,
 };
+sub duration_names{ $duration_names }
+
+my $note_names = [ qw/ c cs d ds e f fs g gs a as b / ];
+sub note_names { $note_names }
 
 my $pitch_nums->@{ 0..127 } = map {
     my $octave = $_;
     map {
         "$_$octave"
-    } qw/ c cs d ds e f fs g gs a as b /
+    } $note_names->@*
 } -2..8;
 
 my $pitch_names = { reverse $pitch_nums->%* };
@@ -52,23 +57,30 @@ my $note_aliases = {
     'bf' => 'as',
     'bb' => 'as',
 };
+sub note_aliases { $note_aliases }
 
 # in quarter notes
-sub duration( $duration ) {
-    my $msg = "Apparent zero duration : $duration";
-    $duration =~ s/_//g;
+sub duration( $duration, $n, $d ) {
+    my ( $count, $dur ) = $duration =~ /([0-9]+)?\s*([.a-z]+)/i;
+    return cfg->trigger if $duration eq 'trigger';
     my $dotted = $duration =~ s/^[d.]//;
-    $duration = $duration_names->{ $duration } // $duration * 4;
-    $duration += $duration / 2 if $dotted;
-    carp $msg unless $duration;
-    $duration;
-}
-
-sub duration_ppqn( $duration ) {
-    duration( $duration ) * cgf->ppqn;
+    $count //= 1;
+    my $qn = ( $dur =~ /^bar/i )
+        ? $count * $n * ( 4 / $d )
+        : $count * $duration_names->{ lc $dur };
+    $qn += $qn / 2 if $dotted;
+    carp "Apparent zero duration: $duration" unless $qn;
+    $qn;
 }
 
 sub pitch( $note ) {
-    $pitch_names->{ $note } // $note;
+    return $note if looks_like_number( $note );
+    my ( $midc ) = cfg->middle_c =~ /C([0-9])/i;
+    my $offset = 3 - $midc;
+    my ( $name, $num ) = $note =~ /([a-z#]+)([0-9])/i;
+    $num += $offset;
+    $name = lc $name;
+    $name = $note_aliases->{ $name } if $note_aliases->{ $name };
+    $pitch_names->{ "$name$num" };
 }
 
